439099f01c9a029fb41bd8db0232b7e68f7be68c
E Rasche <hxr@hx42.org>
2017-07-27 13:20:46 +0000

    finish deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-
+git fetch origin --quiet
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @{0})
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @{0} "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
+	# echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
+	# If we need to pull, instead, pull + restart httpd.
+	echo "Deploying!"
+	git pull
+	systemctl restart httpd
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
 else
